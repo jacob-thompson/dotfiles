d5f8005eae6e962dfe5c7a45fd57da9df35fd161
Jacob Thompson <jacobalthompson@gmail.com>
2025-11-23 03:10:09 -0800

    add zig language server protocol

--- dot_config/nvim/lua/plugins/lsp.lua
+++ dot_config/nvim/lua/plugins/lsp.lua
@@ -37,6 +37,7 @@ return {
                 "lua_ls",
                 "clangd",
                 "pylsp",
+                "zls",
             },
             handlers = {
                 clangd = function()
