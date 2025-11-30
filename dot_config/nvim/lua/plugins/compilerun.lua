a681f99aba61bc6db6353a1b3522caaa7ec432b9
Jacob Thompson <jacobalthompson@gmail.com>
2025-11-30 09:31:06 -0800

    delete compilerun

--- dot_config/nvim/lua/plugins/compilerun.lua
+++
@@ -1,8 +0,0 @@
-vim.keymap.set("n", "<F6>", "<Cmd>CompileRun<CR>")
-vim.keymap.set("i", "<F6>", "<Esc><Cmd>CompileRun<CR>")
-vim.keymap.set("v", "<F6>", "<Esc><Cmd>CompileRun<CR>")
-
-return {
-    "jacob-thompson/vim-compilerun",
-    name = "vim-compilerun"
-}
