97fc3c00c834547ea910d6a0767b7d33c211790d
Jacob Thompson <jacobalthompson@gmail.com>
2025-12-28 16:59:51 -0800

    remove unnecessary nest and fix useLspFoldsWithTreesitterFallback

--- dot_config/nvim/lua/plugins/origami.lua
+++ dot_config/nvim/lua/plugins/origami.lua
@@ -1,32 +1,30 @@
 return {
-    {
-        "chrisgrieser/nvim-origami",
-        event = "VeryLazy",
-        opts = {
-            useLspFoldsWithTreesitterFallback = true,
-            pauseFoldsOnSearch = true,
-            foldtext = {
-                enabled = true,
-                padding = 3,
-                lineCount = {
-                    template = "%d lines", -- `%d` is replaced with the number of folded lines
-                    hlgroup = "Comment",
-                },
-                diagnosticsCount = true, -- uses hlgroups and icons from `vim.diagnostic.config().signs`
-                gitsignsCount = true, -- requires `gitsigns.nvim`
+    "chrisgrieser/nvim-origami",
+    event = "VeryLazy",
+    opts = {
+        useLspFoldsWithTreesitterFallback = { enabled = true },
+        pauseFoldsOnSearch = true,
+        foldtext = {
+            enabled = true,
+            padding = 3,
+            lineCount = {
+                template = "%d lines", -- `%d` is replaced with the number of folded lines
+                hlgroup = "Comment",
             },
-            autoFold = {
-                enabled = true,
-                kinds = { "comment", "imports" }, ---@type lsp.FoldingRangeKind[]
-            },
-            foldKeymaps = {
-                setup = true, -- modifies `h`, `l`, and `$`
-                hOnlyOpensOnFirstColumn = false,
-            },
+            diagnosticsCount = true, -- uses hlgroups and icons from `vim.diagnostic.config().signs`
+            gitsignsCount = true, -- requires `gitsigns.nvim`
         },
-        init = function()
-            vim.opt.foldlevel = 99
-            vim.opt.foldlevelstart = 99
-        end,
-    }
+        autoFold = {
+            enabled = true,
+            kinds = { "comment", "imports" }, ---@type lsp.FoldingRangeKind[]
+        },
+        foldKeymaps = {
+            setup = true, -- modifies `h`, `l`, and `$`
+            hOnlyOpensOnFirstColumn = false,
+        },
+    },
+    init = function()
+        vim.opt.foldlevel = 99
+        vim.opt.foldlevelstart = 99
+    end,
 }
