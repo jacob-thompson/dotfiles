65d8aed05c36a74167e5671b8a27e9aa2228f12e
Jacob Thompson <jacobalthompson@gmail.com>
2026-01-03 12:29:16 -0800

    add markview toggle

--- dot_config/nvim/lua/plugins/markview.lua
+++ dot_config/nvim/lua/plugins/markview.lua
@@ -2,4 +2,16 @@ return {
     "OXY2DEV/markview.nvim",
     name = "markview",
     lazy = false,
+    config = function()
+        vim.api.nvim_set_keymap(
+            "n",
+            "<leader>md",
+            "<CMD>Markview<CR>",
+            {
+                desc = "Toggle Markview",
+                noremap = true,
+                silent = true,
+            }
+        )
+    end,
 };
